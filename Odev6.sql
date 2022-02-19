96ca5a5aa510add0cef03d14c4fffb5f47fc2e3f
YagmurrrCan <74114217+YagmurrrCan@users.noreply.github.com>
2022-02-19 22:20:16 +0300

    Update Odev6.sql

--- Odev6.sql
+++ Odev6.sql
@@ -2,16 +2,18 @@
 SELECT AVG(rental_rate) FROM film; 
 
 --film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
-SELECT COUNT(title)
+SELECT COUNT(*)
 FROM film 
 WHERE title 
 ILIKE 'C%';
 
-
-
 --film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
-
-
-
+SELECT MAX(length)
+FROM film 
+WHERE rental_rate=0.99;
 
 --film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+
+SELECT COUNT(DISTINCT replacement_cost)
+FROM film 
+WHERE length>150;
